93470388a00ef90279dd65047978611944ddadfc
LaUrEnTEM <89503662+LaUrEnTEM@users.noreply.github.com>
2021-10-10 21:27:47 +0300

    Add files via upload

--- LaUrEn.lua
+++ LaUrEn.lua
@@ -4419,7 +4419,7 @@ Text = "• *The • 𝗟𝗔𝗨𝗥𝗘𝗡 𝗧𝗘𝗔𝗠 🦅 and programm
 keyboard = {} 
 keyboard.inline_keyboard = {{{text='- KٰaRٍiUsَ ◞ •',url="t.me/Y_3_3"}}}
 local msg_id = msg.id_/2097152/0.5
-https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id='..msg.chat_id_..'&photo=https://t.me/RFRFF&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
+https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id='..msg.chat_id_..'&photo=https://t.me/Y_3_3&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 return false
 end
 --     Source LaUrEn     --
